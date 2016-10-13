4c52c2fe9087c3a8e7193704966587405a0b4bb0
RyanFrenkenDeltion <97040597@st.deltion.nl>
2016-10-13 09:16:40 +0200

    update

---
+++ Periode 1/Punkball/Main scene/Main scene/Assets/Deathzone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deathzone : MonoBehaviour {
+
+    void OnCollisionEnter(Collision c)
+    {
+        Destroy(c.gameObject);
+
+    }
+
+
+}
