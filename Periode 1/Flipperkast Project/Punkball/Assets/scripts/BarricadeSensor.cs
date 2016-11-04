b4ab3c61d7b8ccbe2c78996a6268f43df72c3c69
RyanFrenkenDeltion <97040597@st.deltion.nl>
2016-11-04 11:02:05 +0100

    Gefixt?

---
+++ Periode 1/Flipperkast Project/Punkball/Assets/scripts/BarricadeSensor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarricadeSensor : MonoBehaviour {
+
+    public GameObject[] obstruction; //lijst van GameObjects
+    private bool visible = true;
+
+    void OnTriggerEnter(Collider x)
+    {
+        print(1);
+        if (visible) //zet alles onzichtbaar
+        {
+            for (int i = 0; i < obstruction.Length; i++) //i is eerst 0, dan 1 enz enz tot de lengte van de lijst
+            {
+                obstruction[i].SetActive(false); //de i (zoveelste) uit de lijst pakken
+            }
+        }
+        else //zet alles zichtbaar
+        {
+            for (int i = 0; i < obstruction.Length; i++)
+            {
+                obstruction[i].SetActive(true);
+            }
+        }
+
+        visible = !visible; //als zichtbaar dan onzichtbaar, als onzichtbaar dan zichtbaar
+    }
+}
---
+++ Periode 1/Flipperkast Project/Punkball/Assets/scripts/Target.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {   
+    [HideInInspector]
+    public bool activated;
+    public GameObject[] targets;
+
+	void OnCollisionEnter(Collision x)
+    {
+        if(x.transform.tag == "Ball")
+        {
+            activated = true;
+        }
+        bool combo = true;
+        foreach (GameObject target in transform.parent.GetComponent<Target>().targets)
+        {
+            if (target.GetComponent<Target>().activated == false)
+            {
+                combo = false;
+            }
+        }
+        if(combo == true)
+        {
+            //hier kan je een combo reward geven
+        }
+    }
+}
